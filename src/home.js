29f2d6cc3bbce2f8536ccb0f8067728afcdfb5dc
Derek Nunes <dereknunes28@gmail.com>
2022-04-24 23:38:38 -0500

    add home.js, other changes made for testing setup.

---
+++ src/home.js
@@ -0,0 +1,20 @@
+import Test from './asssets/img/test.png';
+
+const testimg = new Image();
+testimg.src = Test;
+
+const myName = (name) => {
+    // 'Hi! My name is ' + name;
+    // let element = document.createElement('div');
+    document.innerHTML = `
+    <img src="${testimg}" alt="">
+    `;
+}
+
+const img = () => {
+    
+}
+
+
+
+export default {myName, img};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 import _ from 'lodash';
+// import Test from './test.png';
 import './style.css';
-import Test from './test.png';
+import myName from './home';
+import img from './home';
+
+import Test from './asssets/img/test.png';
+
+const testimg = new Image();
+testimg.src = Test;
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, new imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+  const element = document.createElement('div');
+  // const newelement = document.createElement('div');
+  // use your function!
+  // element.textContent = myName('Cody');
+  element.innerHTML = `
+  <img class="hello" src="../src/asssets/img/test2.png" alt="">
+  <div class="hello">border</div>
+  `
+  // newelement.innerHTML = img();
+  return element;
+}
 
-    // add image to our existing div.
-    const test = new Image();
-    test.src = Test;
+document.body.appendChild(component());
 
-    element.appendChild(test);
+
+
+
+// function component() {
+//     const element = document.createElement('div');
   
-    return element;
-  }
+//     // Lodash, new imported by this script
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//     element.classList.add('hello');
+
+//     // add image to our existing div.
+//     const test = new Image();
+//     test.src = Test;
+
+//     element.appendChild(test);
   
-  document.body.appendChild(component());
+//     return element;
+//   }
+  
+//   document.body.appendChild(component());
